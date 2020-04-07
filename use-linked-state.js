263158e62c94985d9e1edbb7272b6785e5da31fc
Makan <makan.edrisi@gmail.com>
2020-04-07 19:53:28 +0800

    Version 1.0.0

---
+++ use-linked-state.js
@@ -0,0 +1,52 @@
+/**
+ * This is a React hook for sharing data between components.
+ *
+ * @link   https://github.com/makannew/use-linked-state
+ * @file   use-linked-state.js
+ * @author Makan Edrisi
+ * @since  2020
+ * @version 1.0.0
+ */
+import { useEffect, useState, useRef } from "react";
+
+export function useStateGateway(initialState) {
+  const gateway = useRef({ members: [], initialState });
+  useEffect(() => {
+    return () => {
+      gateway.current = { members: [], initialState };
+    };
+  }, []);
+  return gateway.current;
+}
+
+export function useLinkedState(gateway) {
+  const [state, setState] = useState(gateway?.initialState);
+  const notFirstRun = useRef();
+
+  useEffect(() => {
+    const thisMember = { state, setState };
+    gateway.members.push(thisMember);
+    return () => {
+      gateway.members.splice(gateway.members.indexOf(thisMember), 1);
+    };
+  }, []);
+
+  useEffect(() => {
+    for (let member of gateway.members) {
+      if (member.setState !== setState && notFirstRun.current) {
+        member.setState(state);
+      }
+    }
+    if (!notFirstRun.current) {
+      if (
+        gateway.members.length > 0 &&
+        gateway.members[0].setState !== setState
+      ) {
+        setState(gateway.members[0].state);
+      }
+      notFirstRun.current = true;
+    }
+  }, [state]);
+
+  return [state, setState];
+}
